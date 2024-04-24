856a5b1714707036a5f9b05accff5d193e6f3d2a
20019181 <161727068+20019181@users.noreply.github.com>
2024-04-24 16:35:42 +0200

    Add files via upload

---
+++ Untitled20.c
@@ -0,0 +1,58 @@
+#include<iostream>
+
+using namespace std;
+
+int main () {
+//function prototypes
+int add(int,)
+int substract(int,int)
+int multiply(int,int)
+int divide(int,int)
+int modulus (int,int)
+
+int main ()
+int choice, int num1,num2;
+char cont;
+// display menu
+cout<<"1.Add"<<endl;
+cout<<"2.substract"<<endl;
+cout<<"3.multiply"<<endl;
+cout<<"4.divide"<<endl;
+cout<<"5.modulus"<<endl;
+cout<<"enter choice(1-5)"<<endl
+cin>>choice;
+
+int (choice)
+case 1:
+cout<<"enter two integrs"<<endl
+cin>>num1>>num2;
+cout<<"result:"<<add(num1,num2)<<endl;
+
+case 2:
+cout<<"enter two integers: ";
+cin>>num1>>num2;
+cout<<"result: "<<substract (num1,num2)<<endl;
+
+case 3
+cout<<"enter two integers: "<<endl;
+cin>> num1>>num2;
+cout<< "result: "<<multiply(num1,num2)<<endl;
+
+case 4
+cout<<"enter two integers:"<<endl
+cin>> num1 >>num2;
+if (num2==o){
+cout<<"the second integer is zero,divide by zero!"<<endl;}
+else{
+cout<<"result:"<<divide (num1, num2)<<endl;
+}
+
+defaultfloat:
+cout<<"invalid choice!" <<endl;
+}
+cout<<"press'y' or 'Y' to continue: ";
+cin>>cont;
+}while (cont=='y'// cont=='Y');
+
+return 0;
+}
